e8808f562115b8e5352eabbf797411b74d23c00a
karthikdamanellore.viljetech@gmail.com <KarthikDamanellore>
2024-05-30 11:28:16 +0530

    modified header page section

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import Avatar from "@mui/material/Avatar";
 import { deepOrange, deepPurple } from "@mui/material/colors";
@@ -11,111 +10,120 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import { menuData } from "../../constants/index";
 import {
-    headerContainerStyles,
-    popOverStyles,
-    modalStyles,
+  headerContainerStyles,
+  popOverStyles,
+  modalStyles,
 } from "../header/styles";
-import logo from "../../assets/images/avatar.svg";
+import logo from "../../assets/images/avatar(2).svg";
+import Chip from "@mui/material/Chip";
+import profileImg from "../../assets/images/brother.jpg";
 
 const Header = (props) => {
-    // props
-    const { setMenuItem, content, openModal, setOpenModal, customModalStyles } =
-        props;
+  // props
+  const { setMenuItem, content, openModal, setOpenModal, customModalStyles } =
+    props;
 
-    // states:
-    const [anchorEl, setAnchorEl] = useState(null);
-    const open = Boolean(anchorEl);
-    const id = open ? "simple-popover" : undefined;
+  // states:
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
 
-    const handleFileDownload = (url) => {
-        const link = document.createElement("a");
-        link.href = url;
-        link.download = url.split("/").pop();
-        link.click();
-    };
+  const handleFileDownload = (url) => {
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = url.split("/").pop();
+    link.click();
+  };
 
-    const renderLeftSection = () => {
-        return (
-            <img src={logo} alt="logo"  
-            style={{width:"100px", height:"100px",borderRadius: "50%"}}
-             />
-        );
-    };
-
-    const renderRightSection = () => {
-        return (
-            // <Avatar
-            //     sx={avartarStyles}
-            //     alt="Adarsh Damanellore"
-            //     src="/static/images/avatar/1.jpg"
-            //     onClick={(event) => setAnchorEl(event.currentTarget)}
-            // />
-            <Avatar
-                sx={{ bgcolor: deepOrange[500], cursor: "pointer" }}
-                onClick={(event) => setAnchorEl(event.currentTarget)}>
-                A
-            </Avatar>
-        );
-    };
-
-    const renderPopupSection = () => {
-        return (
-            <Popover
-                id={id}
-                open={open}
-                anchorEl={anchorEl}
-                onClose={() => setAnchorEl(null)}
-                anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                }}>
-                <Box sx={popOverStyles}>
-                    {menuData?.map((item, index) => {
-                        return (
-                            <List
-                                key={index}
-                                onClick={() => {
-                                    if (index === 3) {
-                                        handleFileDownload(item.file);
-                                    } else {
-                                        setAnchorEl(null);
-                                        setMenuItem(item?.title);
-                                        setOpenModal(true);
-                                    }
-                                }}>
-                                <ListItem disablePadding>
-                                    <ListItemButton>
-                                        <ListItemIcon>
-                                            {item?.icon}
-                                        </ListItemIcon>
-                                        <ListItemText primary={item?.title} />
-                                    </ListItemButton>
-                                </ListItem>
-                            </List>
-                        );
-                    })}
-                </Box>
-            </Popover>
-        );
-    };
-
-    const renderModalSection = () => {
-        return (
-            <Modal open={openModal} onClose={() => setOpenModal(false)}>
-                <Box sx={{ ...modalStyles, ...customModalStyles }}>
-                    {content}
-                </Box>
-            </Modal>
-        );
-    };
+  const renderLeftSection = () => {
     return (
-        <Box sx={headerContainerStyles}>
-            {renderLeftSection()}
-            {renderRightSection()}
-            {renderPopupSection()}
-            {renderModalSection()}
-        </Box>
+      <img
+        src={logo}
+        alt="logo"
+        style={{ width: "100px", height: "100px", borderRadius: "50%" }}
+      />
     );
+  };
+
+  const renderRightSection = () => {
+    return (
+      // <Avatar
+      //     sx={avartarStyles}
+      //     alt="Adarsh Damanellore"
+      //     src="/static/images/avatar/1.jpg"
+      //     onClick={(event) => setAnchorEl(event.currentTarget)}
+      // />
+      // <Avatar
+      //   sx={{ bgcolor: deepOrange[500], cursor: "pointer" }}
+      //   onClick={(event) => setAnchorEl(event.currentTarget)}
+      // >
+      //   A
+      // </Avatar>
+      <Chip
+        avatar={<Avatar alt="Adarsh" src={profileImg} />}
+        label="Explore my profile"
+        color="warning"
+        onClick={(event) => setAnchorEl(event.currentTarget)}
+      />
+    );
+  };
+
+  const renderPopupSection = () => {
+    return (
+      <Popover
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Box sx={popOverStyles}>
+          {menuData?.map((item, index) => {
+            return (
+              <List
+                key={index}
+                onClick={() => {
+                  if (index === 3) {
+                    handleFileDownload(item.file);
+                  } else {
+                    setAnchorEl(null);
+                    setMenuItem(item?.title);
+                    setOpenModal(true);
+                  }
+                }}
+              >
+                <ListItem disablePadding>
+                  <ListItemButton>
+                    <ListItemIcon>{item?.icon}</ListItemIcon>
+                    <ListItemText primary={item?.title} />
+                  </ListItemButton>
+                </ListItem>
+              </List>
+            );
+          })}
+        </Box>
+      </Popover>
+    );
+  };
+
+  const renderModalSection = () => {
+    return (
+      <Modal open={openModal} onClose={() => setOpenModal(false)}>
+        <Box sx={{ ...modalStyles, ...customModalStyles }}>{content}</Box>
+      </Modal>
+    );
+  };
+  return (
+    <Box sx={headerContainerStyles}>
+      {renderLeftSection()}
+      {renderRightSection()}
+      {renderPopupSection()}
+      {renderModalSection()}
+    </Box>
+  );
 };
 
 export default Header;
--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -6,16 +6,20 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { contactUsData, educationData, skillsData } from "../../constants/index";
 import {
-    cardStyles,
-    contactItemTextStyles,
-    customModalStyles,
-    educationBlockStyles,
-    educationHeadingStyles,
-    mySelfContainerStyles,
-    selfContentDescStyles,
-    skillsModalStyles,
+  contactUsData,
+  educationData,
+  skillsData,
+} from "../../constants/index";
+import {
+  cardStyles,
+  contactItemTextStyles,
+  customModalStyles,
+  educationBlockStyles,
+  educationHeadingStyles,
+  mySelfContainerStyles,
+  selfContentDescStyles,
+  skillsModalStyles,
 } from "../../pages/portfolio/styles";
 import { Typewriter } from "react-simple-typewriter";
 import { Button, Typography } from "@mui/material";
@@ -33,238 +37,250 @@ import "react-multi-carousel/lib/styles.css";
 import styles from "./global.module.css";
 
 const PortFolioApp = () => {
-    const [menuItem, setMenuItem] = useState(null);
-    const [openModal, setOpenModal] = useState(false);
-    const [expandedAccordion, setExpandedAccordion] = useState(0);
-    const [isMobile, setIsMobile] = useState(null);
+  const [menuItem, setMenuItem] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [expandedAccordion, setExpandedAccordion] = useState(0);
+  const [isMobile, setIsMobile] = useState(null);
 
-    const getContent = () => {
-        switch (menuItem) {
-            case "My Self":
-                return renderMySelfSection();
-            case "Education":
-                return renderEducationSection();
-            case "Skills":
-                return renderSkillsSection();
-            case "Contact":
-                return renderContactSection();
-        }
+  const getContent = () => {
+    switch (menuItem) {
+      case "My Self":
+        return renderMySelfSection();
+      case "Education":
+        return renderEducationSection();
+      case "Skills":
+        return renderSkillsSection();
+      case "Contact":
+        return renderContactSection();
+    }
+  };
+
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 510);
     };
 
-    const responsive = {
-        superLargeDesktop: {
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5,
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        },
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
     };
+  }, []);
 
-    useEffect(() => {
-        const handleResize = () => {
-            setIsMobile(window.innerWidth <= 510);
-        };
-
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    const renderBannerSection = () => {
-        return (
-            <div className={styles.bannerTextStyles}>
-                <h1
-                    style={{
-                        paddingTop: "5rem",
-                        margin: "auto 0",
-                        fontWeight: "normal",
-                        textAlign: "center",
-                    }}>
-                    Hi, Iam Adarsh Web Developer ready to <br />
-                    <span style={{ color: "red", fontWeight: "bold" }}>
-                        <Typewriter
-                            words={[
-                                "Innovate",
-                                "Collaborate",
-                                "Develop",
-                                "Succeed!",
-                            ]}
-                            cursor
-                            cursorStyle="|"
-                            typeSpeed={70}
-                            deleteSpeed={70}
-                            delaySpeed={1000}
-                            loop={false}
-                        />
-                    </span>
-                </h1>
-            </div>
-        );
-    };
-
-    const renderMySelfSection = () => {
-        return (
-            <>
-                <Box sx={mySelfContainerStyles}>
-                    <img src={mySelfImg} alt="mySelfImg"  style={{width:"150px", height:"150px"}} className={styles.mySelfImageStyles}/>
-                    <div className={styles.selfContentBlockStyles}>
-                        <Typography variant="h4" sx={selfContentDescStyles}>
-                            About Me
-                        </Typography>
-                        <Typography sx={selfContentDescStyles}>
-                            I am Adarsh, a passionate front-end developer with a
-                            keen eye for detail and a love for creating
-                            engaging, user-friendly web interfaces. As a recent
-                            graduate, I have honed my skills in HTML, CSS,
-                            JavaScript, and React. I am eager to bring my fresh
-                            perspective and technical expertise to a dynamic
-                            team, contributing to and growing within it.
-                        </Typography>
-                    </div>
-                </Box>
-                {renderCloseBtnSection()}
-            </>
-        );
-    };
-
-    const renderEducationSection = () => {
-        return (
-            <>
-                <Box sx={educationBlockStyles}>
-                    {educationData?.map((item, index) => {
-                        return (
-                            <Accordion
-                                key={index}
-                                expanded={expandedAccordion === index}
-                                onChange={() =>
-                                    setExpandedAccordion(
-                                        expandedAccordion === index
-                                            ? null
-                                            : index
-                                    )
-                                }>
-                                <AccordionSummary
-                                    sx={educationHeadingStyles}
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls="panel1-content"
-                                    id="panel1-header">
-                                    
-                                    <img src={item?.icon} alt="item?.icon" style={{width:"40px", height:"40px"}} className={styles.educationImgStyles} />
-                                    {item?.graduation} - {item?.aggregate}
-                                </AccordionSummary>
-                                <AccordionDetails sx={educationHeadingStyles}>
-                                    {item?.instituteName}
-                                </AccordionDetails>
-                                <AccordionDetails sx={educationHeadingStyles}>
-                                    {item?.year}
-                                </AccordionDetails>
-                            </Accordion>
-                        );
-                    })}
-                </Box>
-                {renderCloseBtnSection()}
-            </>
-        );
-    };
-
-    const renderSkillsSection = () => {
-        return (
-            <>
-                <Carousel
-                    responsive={responsive}
-                    keyBoardControl={true}
-                    swipeable={true}
-                    infinite={true}
-                    showDots={isMobile}
-                    arrows={!isMobile}>
-                    {skillsData?.map((item, index) => {
-                        return (
-                            <Card key={index} sx={cardStyles}>
-                                <CardActionArea>
-                                    <CardContent>
-                                        <img src={item?.image} style={{width:"100px", height:"100px", objectFit:"contain"}} alt={item?.title} className={styles.imageStyles}/>
-                                        <Typography gutterBottom variant="h5">
-                                            {item?.title}
-                                        </Typography>
-                                        <Typography
-                                            variant="body2"
-                                            color="text.secondary">
-                                            {item?.description}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        );
-                    })}
-                </Carousel>
-                {renderCloseBtnSection()}
-            </>
-        );
-    };
-
-    const renderContactSection = () => {
-        return (
-            <>
-                {contactUsData?.map((item, index) => (
-                    <List key={index}>
-                        <ListItem disablePadding>
-                            <ListItemButton
-                                onClick={() =>
-                                    window.open(item.link, "_blank")
-                                }>
-                                <ListItemIcon>{item?.icon}</ListItemIcon>
-                                <ListItemText
-                                    primary={item?.title}
-                                    sx={contactItemTextStyles}
-                                />
-                            </ListItemButton>
-                        </ListItem>
-                    </List>
-                ))}
-                {renderCloseBtnSection()}
-            </>
-        );
-    };
-
-    const renderCloseBtnSection = () => {
-        return (
-            <Button
-                variant="contained"
-                color="error"
-                sx={{ display: "flex", marginLeft: "auto", marginTop: "10px" }}
-                onClick={() => setOpenModal(false)}>
-                Close
-            </Button>
-        );
-    };
-
+  const renderBannerSection = () => {
     return (
-        <Box>
-            <Header
-                setMenuItem={setMenuItem}
-                openModal={openModal}
-                setOpenModal={setOpenModal}
-                content={getContent()}
-                customModalStyles={
-                    menuItem === "Education"
-                        ? customModalStyles
-                        : menuItem === "Skills" && skillsModalStyles
-                }
+      <div className={styles.bannerContainerStyles}>
+        <h1
+          style={{
+            fontWeight: "normal",
+            textAlign: "center",
+          }}
+          className={styles.bannerTextStyles}
+        >
+          Hi, Iam Adarsh Web Developer <br /> ready to &nbsp;
+          <span
+            style={{ color: "red", fontWeight: "bold" }}
+            className={styles.bannerTextStyles}
+          >
+            <Typewriter
+              words={["Innovate", "Collaborate", "Develop", "Succeed!"]}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={70}
+              delaySpeed={1000}
+              loop={false}
             />
-            {renderBannerSection()}
-        </Box>
+          </span>
+        </h1>
+      </div>
     );
+  };
+
+  const renderMySelfSection = () => {
+    return (
+      <>
+        <Box sx={mySelfContainerStyles}>
+          <img
+            src={mySelfImg}
+            alt="mySelfImg"
+            style={{ width: "150px", height: "150px" }}
+            className={styles.mySelfImageStyles}
+          />
+          <div className={styles.selfContentBlockStyles}>
+            <Typography variant="h4" sx={selfContentDescStyles}>
+              About Me
+            </Typography>
+            <Typography sx={selfContentDescStyles}>
+              I am Adarsh, a passionate front-end developer with a keen eye for
+              detail and a love for creating engaging, user-friendly web
+              interfaces. As a recent graduate, I have honed my skills in HTML,
+              CSS, JavaScript, and React. I am eager to bring my fresh
+              perspective and technical expertise to a dynamic team,
+              contributing to and growing within it.
+            </Typography>
+          </div>
+        </Box>
+        {renderCloseBtnSection()}
+      </>
+    );
+  };
+
+  const renderEducationSection = () => {
+    return (
+      <>
+        <Box sx={educationBlockStyles}>
+          {educationData?.map((item, index) => {
+            return (
+              <Accordion
+                key={index}
+                expanded={expandedAccordion === index}
+                onChange={() =>
+                  setExpandedAccordion(
+                    expandedAccordion === index ? null : index
+                  )
+                }
+              >
+                <AccordionSummary
+                  sx={educationHeadingStyles}
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  <img
+                    src={item?.icon}
+                    alt="item?.icon"
+                    style={{ width: "40px", height: "40px" }}
+                    className={styles.educationImgStyles}
+                  />
+                  {item?.graduation} - {item?.aggregate}
+                </AccordionSummary>
+                <AccordionDetails sx={educationHeadingStyles}>
+                  {item?.instituteName}
+                </AccordionDetails>
+                <AccordionDetails sx={educationHeadingStyles}>
+                  {item?.year}
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+        </Box>
+        {renderCloseBtnSection()}
+      </>
+    );
+  };
+
+  const renderSkillsSection = () => {
+    return (
+      <>
+        <Carousel
+          responsive={responsive}
+          keyBoardControl={true}
+          swipeable={true}
+          infinite={true}
+          showDots={isMobile}
+          arrows={!isMobile}
+        >
+          {skillsData?.map((item, index) => {
+            return (
+              <Card key={index} sx={cardStyles}>
+                <CardActionArea>
+                  <CardContent>
+                    <img
+                      src={item?.image}
+                      style={{
+                        width: "100px",
+                        height: "100px",
+                        objectFit: "contain",
+                      }}
+                      alt={item?.title}
+                      className={styles.imageStyles}
+                    />
+                    <Typography gutterBottom variant="h5">
+                      {item?.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {item?.description}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            );
+          })}
+        </Carousel>
+        {renderCloseBtnSection()}
+      </>
+    );
+  };
+
+  const renderContactSection = () => {
+    return (
+      <>
+        {contactUsData?.map((item, index) => (
+          <List key={index}>
+            <ListItem disablePadding>
+              <ListItemButton onClick={() => window.open(item.link, "_blank")}>
+                <ListItemIcon>{item?.icon}</ListItemIcon>
+                <ListItemText
+                  primary={item?.title}
+                  sx={contactItemTextStyles}
+                />
+              </ListItemButton>
+            </ListItem>
+          </List>
+        ))}
+        {renderCloseBtnSection()}
+      </>
+    );
+  };
+
+  const renderCloseBtnSection = () => {
+    return (
+      <Button
+        variant="contained"
+        color="error"
+        sx={{ display: "flex", marginLeft: "auto", marginTop: "10px" }}
+        onClick={() => setOpenModal(false)}
+      >
+        Close
+      </Button>
+    );
+  };
+
+  return (
+    <Box>
+      <Header
+        setMenuItem={setMenuItem}
+        openModal={openModal}
+        setOpenModal={setOpenModal}
+        content={getContent()}
+        customModalStyles={
+          menuItem === "Education"
+            ? customModalStyles
+            : menuItem === "Skills" && skillsModalStyles
+        }
+      />
+      {renderBannerSection()}
+    </Box>
+  );
 };
 
 export default PortFolioApp;
